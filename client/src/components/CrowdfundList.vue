3ec618d778676ac4f5a2b2d4b5bd0365334149bd
imnotjs <134348800+imnotjs@users.noreply.github.com>
2024-12-22 10:22:42 +0700

    Create CrowdfundList.vue

---
+++ client/src/components/CrowdfundList.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<h1>Available Crowdfunds</h1>
+		<div v-for="crowdfund in crowdfunds" :key="crowdfund._id" class="crowdfund-item">
+			<h3>{{ crowdfund.name }}</h3>
+			<p>Target: {{ crowdfund.target }}</p>
+			<router-link :to="`/crowdfund/${crowdfund._id}`">View Details</router-link>
+		</div>
+	</div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+	data() {
+		return {
+			crowdfunds: [],
+		};
+	},
+	async created() {
+		const response = await axios.get("/api/crowdfund");
+		this.crowdfunds = response.data;
+	},
+};
+</script>
