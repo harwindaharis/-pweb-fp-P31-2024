7ed9ea31ea169f16fdef33b9949539193e8cf8cd
imnotjs <134348800+imnotjs@users.noreply.github.com>
2024-12-22 10:19:55 +0700

    Update crowdfund.model.js

--- server/src/features/crowdfund/crowdfund.model.js
+++ server/src/features/crowdfund/crowdfund.model.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 
 const CrowdfundSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  target: { type: Number, required: true },
-  status: { type: String, enum: ['open', 'close'], default: 'open' },
+	name: { type: String, required: true },
+	target: { type: Number, required: true },
+	status: { type: String, enum: ["open", "close"], default: "open" },
+	favorite: { type: Boolean, default: false },
+	comments: [
+		{
+			user: { type: String },
+			message: { type: String, required: true },
+			createdAt: { type: Date, default: Date.now },
+		},
+	],
 });
 
 module.exports = mongoose.model('Crowdfund', CrowdfundSchema);
